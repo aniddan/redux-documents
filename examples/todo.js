6bac88119e2e75228b98d5723ac5e60eee0c0bc2
Iddan Aharonson <mail@aniddan.com>
2017-02-07 19:08:32 +0200

    Fix addition and example

--- examples/todo.js
+++ examples/todo.js
@@ -1,22 +1,22 @@
 import { createStore } from 'redux';
-import Collection from '.';
+import Collection from '..';
 
-function todo(state = [], action) {
-	return Collection({
-		INSERT: 'ADD_TODO',
-		UPDATE: 'CHANGE_TODO',
-		DELETE: 'REMOVE_TODO',
-		compare: (a, b) => a._id == b._id,
-	})(state, action);
-}
+const todo = (state = [], action) =>
+  Collection({
+    INSERT: 'ADD_TODO',
+    UPDATE: 'CHANGE_TODO',
+    DELETE: 'REMOVE_TODO',
+    compare: (a, b) => a._id == b._id,
+  })(state, action);
 
-let store = createStore(todo, [
-	{
-		_id: '1',
-		text: 'Use Redux',
-	}
+const store = createStore(todo, [
+  {
+    _id: '1',
+    text: 'Use Redux',
+  },
 ]);
 
+console.log(store.getState());
 store.subscribe(() => console.log(store.getState()));
 
 store.dispatch({
@@ -24,20 +24,20 @@ store.dispatch({
   payload: {
     _id: '2',
     text: 'Read the docs',
-  }
+  },
 });
 
 store.dispatch({
-	type: 'CHANGE_TODO',
-	payload: {
-		_id: '2',
-		text: 'READ the docs',
-	}
+  type: 'CHANGE_TODO',
+  payload: {
+    _id: '2',
+    text: 'READ the docs',
+  },
 });
 
 store.dispatch({
-	type: 'REMOVE_TODO',
-	payload: {
-		_id: '1'
-	}
+  type: 'REMOVE_TODO',
+  payload: {
+    _id: '1',
+  },
 });
--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 export default function Collection({ INSERT, UPDATE, DELETE, compare, extract = action => action.payload }) {
-	return (state, action) => {
-		switch (action.type) {
-		case INSERT: {
-			return [...state, ...extract(action)];
-		}
-		case UPDATE: {
-			let changes = extract(action);
-			return state.map(doc => {
-				if (compare(doc, changes)) {
-					return {...doc, ...changes};
-				}
-				return doc;
-			});
-		}
-		case DELETE: {
-			return state.filter(doc => compare(doc, extract(action)));
-		}
-		}
-		return state;
-	};
+  return (state, action) => {
+    const extracted = extract(action);
+    switch (action.type) {
+      case INSERT: {
+        return [...state, ...(extracted[Symbol.iterator] ? extracted : [extracted])];
+      }
+      case UPDATE: {
+        return state.map(doc => {
+          if (compare(doc, extracted)) {
+            return { ...doc, ...extracted };
+          }
+          return doc;
+        });
+      }
+      case DELETE: {
+        return state.filter(doc => compare(doc, extracted));
+      }
+    }
+    return state;
+  };
 }
